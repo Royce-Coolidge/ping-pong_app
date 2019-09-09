643e018f99e0989e0a77f2bf0636818e58f60e14
Rowley thompson <thompsonrjwt.gmail.com>
2019-09-09 16:51:07 +0100

    scorePlayer1, handleIncrementPlayer1, scorePlayer2, handleIncrementPlayer2, reset, server, winner

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = ( {scorePlayer1, handleIncrementPlayer1, scorePlayer2, handleIncrementPlayer2, reset, server, winner} ) => (
+    <React.Fragment>
+        {/* header */}
+        <header className="page-header">
+            <h1>PongPing</h1>
+        </header>
+
+        {/* scores */}
+        <div className="row">
+            <div className="col-xs-6">
+                <p>Player 1 { server === 0 ? <span className="pull-right label label-success">Serving</span> : null }</p>
+                <p className="well">{ scorePlayer1 }</p>
+                <button  onClick={ handleIncrementPlayer1 } className="btn btn-primary">+</button>
+            </div>
+
+            <div className="col-xs-6">
+                <p>Player 2 { server === 1 ? <span className="pull-right label label-success">Serving</span> : null }</p>
+                <p className="well">{ scorePlayer2 }</p>
+                <button onClick={ handleIncrementPlayer2 } className="btn btn-primary">+</button>
+            </div>
+        </div>
+
+        { winner !== 0 ?
+        <h2 className="jumbotron">Player { winner } wins!</h2> : null
+        }
+
+        <hr />
+
+        { /* reset button */}
+        <button onClick={ reset } className="btn btn-danger">Reset</button>
+    </React.Fragment>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,71 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//initial state
+const initial = {
+    player1: 0,
+    player2: 0,
+    server: 0,
+    winner: 0,
+};
+
+
+const winner = state => {
+    return{
+         ...state,
+        winner:
+            state.player1 > 20 ? 1 : 
+            state.player2 > 20 ? 2 :
+            0,
+    }
+};
+
+const setServer = state => {
+let total = state.player1 + state.player2;
+return { ...state, server: Math.floor(total / 5) % 2, }
+};
+
+const incrementScore= (state, player) => {
+    return { ...state, [player]: state[player] + 1, }
+}
+
+
+
+const reducer = (state, action) => {
+   switch (action.type) {
+    // using object spread to create a new object
+     case "incrementScore": return winner(setServer(incrementScore(state, action.player))) 
+     case "reset": return initial;
+     default: return state;
+    } 
+};
+
+const store = createStore(
+    reducer,
+    initial,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+// subscribe to any changes
+let render = () => {
+    let state = store.getState();
+    ReactDOM.render(
+    <App
+    scorePlayer1 ={ state.player1 }
+    scorePlayer2 ={ state.player2 }
+    server ={ state.server }
+    winner ={ state.winner }
+    handleIncrementPlayer1 = { () => store.dispatch({ type: "incrementScore", player: "player1" })}
+    handleIncrementPlayer2 = { () => store.dispatch({ type: "incrementScore", player: "player2" })}
+    reset ={() => store.dispatch({ type: "reset" }) }
+    />, document.getElementById('root'));
+};
+
+store.subscribe(render);
+
+render();
+
+// dispatching an action

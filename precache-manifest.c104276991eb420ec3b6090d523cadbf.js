7fe3b1a3f89613d98d85fa8a4d855c31b3992e3d
Rowley thompson <thompsonrjwt.gmail.com>
2019-09-30 19:12:01 +0100

    Updates

---
+++ precache-manifest.c104276991eb420ec3b6090d523cadbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0faacbc28335f3882c50c3825119cdb2",
+    "url": "/ping-pong_app/index.html"
+  },
+  {
+    "revision": "0601fb78540f05dd2b64",
+    "url": "/ping-pong_app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3c1a1e99edfb58039020",
+    "url": "/ping-pong_app/static/js/2.6080e0d3.chunk.js"
+  },
+  {
+    "revision": "0601fb78540f05dd2b64",
+    "url": "/ping-pong_app/static/js/main.4d2a466c.chunk.js"
+  },
+  {
+    "revision": "564c10e7cb8639601f1f",
+    "url": "/ping-pong_app/static/js/runtime~main.27710700.js"
+  }
+]);
